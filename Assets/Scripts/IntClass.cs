06ba4f262a674b88a8ff51e474eebc431cb797de
redasdm123 <64580710+redasdm123@users.noreply.github.com>
2020-10-20 14:19:03 +0900

    First

---
+++ Assets/Scripts/IntClass.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class IntClass: MonoBehaviour
+{
+    public static int count = 0;
+    Text text;
+    private void Start()
+    {
+        text=GetComponent<Text>();
+    }
+    private void Update()
+    {
+        text.text = count.ToString();
+    }
+}
