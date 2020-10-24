00d99fb4fb5d53f60e542716fabe37c5b5c69702
redasdm123 <64580710+redasdm123@users.noreply.github.com>
2020-10-24 15:19:52 +0900

    공개코드

---
+++ Assets/Scripts/LamdaWait/WorkFinish.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WorkFinish : MonoBehaviour
+{
+    WorkFlow workFlow;
+    Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+        workFlow = FindObjectOfType<WorkFlow>();
+        MakeWork();
+    }
+
+    void ShowText()
+    {
+        text.text = "완성";
+    }
+
+    void MakeWork()
+    {
+        StartCoroutine(workFlow.Working());
+        ShowText();
+    }
+}
---
+++ Assets/Scripts/LamdaWait/WorkFlow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WorkFlow : MonoBehaviour
+{
+    Slider slider;
+    private void Awake()
+    {
+        slider = GetComponent<Slider>();
+    }
+    public IEnumerator Working()
+    {
+        float t = 0;
+        while(t<1)
+        {
+            slider.value = t;
+            t += 0.01f;
+            yield return null;
+        }
+    }
+}
